4218ed43ec92bc02d460f6920e3d1b770680f312
William and Lady Navas <ladysantoyo@williams-mbp.uh.uhnj.org>
2019-05-23 11:22:02 -0400

    img

--- client/src/components/pages/Weddings.js
+++ client/src/components/pages/Weddings.js
@@ -94,7 +94,7 @@ class Weddings extends React.Component {
                     style={customStyles}
                     contentLabel="Example Modal">
                     <button className="close-btn-modal" onClick={this.closeModal}>close</button>
-                    <div className="modal-img-div"><img style={{width:"100%", height:"100%", objectFit:"contain"}} src={this.state.modalUrl}/></div>
+                    <div className="modal-img-div"><img style={{width:"100%", height:"100%", objectFit:"contain", maxHeight:"700px", maxWidth:"1000px"}} src={this.state.modalUrl}/></div>
                 </Modal>
             </div>
         );
