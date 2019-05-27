96bbd05a27dbb7931345a64e93e766ec307f5841
William and Lady Navas <ladysantoyo@Williams-MacBook-Pro.local>
2019-05-26 20:42:12 -0400

    menu

--- client/src/components/Navbar-user/Navbar-user.js
+++ client/src/components/Navbar-user/Navbar-user.js
@@ -26,20 +26,20 @@ class NavbarUser extends React.Component {
     return (
       <nav className='nav-user'>
 
-        <Link to="/"
+        {/* <Link to="/"
           className="navbar-logo navbar-brand mr-0 mr-md-2 mt-0"
           aria-label="Bootstrap"
           style={{ textDecoration: "none", color: "black" }}>
           <i className="fa fa-home" style={{ marginLeft: "10px", marginTop: "6px", color: "darkGray", fontSize: "30px" }}></i>
-        </Link>
+        </Link> */}
         
        <Link to='/' style={{ textDecoration: "none", color: "rgb(199, 100, 19)" }}><div className="WNP-header"><h3 className="WNP">William Navas Photo</h3></div></Link>
-        <ul>
-          {/* {(!this.props.user ? (<li className="nav-item"> <Link to="/signup" className="nav-link" style={{ textDecoration: "none", color: "black" }}> <p className="nav-hover" style={{ color: 'black' }}><i className="far fa-user"></i>Sign Up</p> </Link> </li>) : "")} */}
-          {(!this.props.user ? (<li className="nav-item1"><Link to="/login-local" className="nav-link" style={{ textDecoration: "none", color: "black" }}> <p className="nav-hover2" style={{ color: 'white' }}><i className="far fa-user"></i> Log In</p> </Link> </li>) : "")}
-          {(this.props.user.name === 'Admin' ? (<li className="nav-item"><Link to="/admin" className="nav-link" style={{ textDecoration: "none", color: "black" }}><p className="nav-hover1" style={{ color: 'white' }}><strong>{this.props.user.name}</strong></p> </Link></li>) : "")}
-          {(this.props.user.name !== 'Admin' ? (<li className="nav-item"><Link to="/usersite" className="nav-link" style={{ textDecoration: "none", color: "black" }}><p className="nav-hover1" style={{ color: 'white', fontSize: "20px" }}><strong>{this.props.user.name}</strong></p> </Link></li>) : "")}
-          {(this.props.user ? (<li className="nav-item1"><Link className="nav-link" style={{ textDecoration: "none", color: "black" }}> <p onClick={this.props.logOut} className="nav-hover2" style={{ color: 'white' }}><i className="far fa-user"></i> Log Out</p> </Link></li>) : "")}
+        <ul className="login-menu">
+          {/* {(!this.props.user ? (<div className="nav-item"> <Link to="/signup" className="nav-link" style={{ textDecoration: "none", color: "black" }}> <p className="nav-hover" style={{ color: 'black' }}><i className="far fa-user"></i>Sign Up</p> </Link> </div>) : "")} */}
+          {(!this.props.user ? (<li className="nav-item"><Link to="/login-local" style={{ textDecoration: "none", color: "black" }}> <p className="nav-hover" style={{ color: 'white' }}><i className="far fa-user"></i> Log In</p> </Link> </li>) : "")}
+          {(this.props.user.name === 'Admin' ? (<li className="nav-item"><Link to="/admin"  style={{ textDecoration: "none", color: "black" }}><p className="nav-hover" style={{ color: 'white' }}><strong>{this.props.user.name}</strong></p> </Link></li>) : "")}
+          {(this.props.user.name !== 'Admin' ? (<li className="nav-item"><Link to="/usersite" style={{ textDecoration: "none", color: "black" }}><p className="nav-hover" style={{ color: 'white', fontSize: "20px" }}><strong>{this.props.user.name}</strong></p> </Link></li>) : "")}
+          {(this.props.user ? (<li className="nav-item"><Link  style={{ textDecoration: "none", color: "black" }}> <p onClick={this.props.logOut} className="nav-hover" style={{ color: 'white' }}><i className="far fa-user"></i> Log Out</p> </Link></li>) : "")}
         </ul>
       </nav>
     )
